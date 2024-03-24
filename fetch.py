0dae9683aa5bf37ffbe7c11eee731d213e6cce6e
Matthew Atkins <108462359+scout0773@users.noreply.github.com>
2024-03-24 00:29:15 -0400

    add valid.txt example, and fix fetch to change how the errors are printed

--- fetch.py
+++ fetch.py
@@ -14,6 +14,12 @@ def fetch_pgp_keys(username):
             return matches[0].strip()
         else:
             return None
+    except requests.exceptions.HTTPError as e:
+        if e.response.status_code == 404:
+            print(f"PGP key not found for {username}")
+        else:
+            print(f"Couldn't fetch PGP key for {username}: {str(e)}")
+        return None
     except Exception as e:
         print(f"Error occurred while fetching PGP key for {username}: {str(e)}")
         return None
