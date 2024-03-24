d57d5b159da36c4852ee7061ea959c82182be1eb
Matthew Atkins <108462359+scout0773@users.noreply.github.com>
2024-03-24 00:22:45 -0400

    adjust main

--- main.py
+++ main.py
@@ -1,38 +1,19 @@
-import requests
-import re
-import base64
-
-def fetch_and_decode_pgp_keys(username):
-    keybase_url = f"https://keybase.io/{username}/pgp_keys.asc"
-    
-    try:
-        response = requests.get(keybase_url)
-        response.raise_for_status()
-        pgp_key_block = response.text
-        pattern = re.compile(r'(?<=-----BEGIN PGP PUBLIC KEY BLOCK-----)(.*?)(?=-----END PGP PUBLIC KEY BLOCK-----)', re.DOTALL)
-        matches = pattern.findall(pgp_key_block)
-        if matches:
-            pgp_key_base64 = matches[0].strip()
-            decoded_pgp_key = base64.b64decode(pgp_key_base64).decode('latin1')
-            email_pattern = re.compile(r'<([^<>]+)>')
-            email_matches = email_pattern.findall(decoded_pgp_key)
-            valid_emails = [email for email in email_matches if '@' in email and re.match(r'^[\w\.-]+@[\w\.-]+$', email)]
-            if valid_emails:
-                with open("valid.txt", "a") as valid_file:
-                    for email in valid_emails:
-                        valid_file.write(f"{username}:{email}\n")
-            else:
-                print(f"No valid email found for {username}.")
-        else:
-            print(f"No PGP key found for {username}.")
-    except Exception as e:
-        print(f"Error occurred while fetching or decoding PGP key for {username}: {str(e)}")
+from fetch import fetch_pgp_keys
+from decode import decode_pgp_keys
+from extract import extract_emails
+from write import write_to_file
 
 def main():
     with open("usernames.txt", "r") as file:
         usernames = file.readlines()
         for username in usernames:
-            fetch_and_decode_pgp_keys(username.strip())
+            username = username.strip()
+            pgp_key_base64 = fetch_pgp_keys(username)
+            if pgp_key_base64:
+                decoded_pgp_key = decode_pgp_keys(pgp_key_base64)
+                if decoded_pgp_key:
+                    valid_emails = extract_emails(decoded_pgp_key)
+                    write_to_file(username, valid_emails)
 
 if __name__ == "__main__":
     main()
